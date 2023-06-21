228d9a0ba2c824b70efd2394b1f1270012becbf9
RawanAlalfi <rawangalalfi@gmail.com>
2023-06-21 23:50:47 +0300

    rawan

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,8 @@
 function Footer() {
   return (
     <>
-<<<<<<< HEAD
     <h1>footer</h1>
-    <h2>rawan</h2>
-=======
-    <h1>footer </h1>
-    <h2>talal</h2>
->>>>>>> 87ebda1b83ad6ab2024925b8926a8aa13524daf8
+<h2>rawan</h2>
     </>
   );
 }
